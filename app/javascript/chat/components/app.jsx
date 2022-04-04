81dc819655e6b5f619ea8f66f0834bc1d01514f6
Jessie <jessie.baxter29@gmail.com>
2022-04-04 14:23:19 +1000

    Initial commit with device tempate and mounting of React chat app

---
+++ app/javascript/chat/components/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ChannelList from '../containers/channel_list';
+import MessageList from '../containers/message_list';
+
+const App = () => {
+  return (
+    <div className="messaging-wrapper">
+      <div className="logo-container">
+      </div>
+      <ChannelList />
+      <MessageList />
+    </div>
+  );
+};
+
+export default App;
